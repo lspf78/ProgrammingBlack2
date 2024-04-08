cefcc3b1b79980ff7a7b025adbf63542f247d751
lspf78 <lspf78@durham.ac.uk>
2024-04-08 19:48:23 +0100

    Add existing file

---
+++ amplify-demo-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api1864b05f": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "CustomerHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ amplify-demo-app/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { get } from 'aws-amplify/api';
+import React, { useEffect, useState } from 'react'
+
+const myAPI = "api1864b05f"
+const path = '/customers'; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [customers, setCustomers] = useState([])
+
+  //Function to fetch from our backend and update customers array
+  async function getCustomer(e) {
+    let customerId = e.input
+    const restOperation = get({apiName: myAPI, path: path + "/" + customerId})
+    const { body } = await restOperation.response;
+    const response = await body.json();
+    console.log(response)
+    let newCustomers = [...customers]
+    newCustomers.push(response)
+    setCustomers(newCustomers)
+  }
+
+  return (
+    
+    <div className="App">
+      <h1>Super Simple React App</h1>
+      <div>
+          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+
+      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       customers.map((thisCustomer, index) => {
+         return (
+        <div key={thisCustomer.customerId}>
+          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+        </div>)
+       })
+      }
+    </div>
+  )
+}
+
+export default App;
